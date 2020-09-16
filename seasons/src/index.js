4f33a396b6306bb47d083e33d66d17b25e8899c1
ankur-jft <ankur.khandelwal@jellyfishtechnologies.com>
2020-09-16 14:29:26 +0530

    Season app completed

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,30 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 class App extends React.Component{
-    constructor(props){
-        super(props);
+        // constructor(props){
+        //     super(props);
+        //     // this is the only time we do direct assignment to this.state
+        //     this.state = { lat: null, errorMessage: '' };
 
-        // this is the only time we do direct assignment
-        // to this.state
-        this.state = { lat: null, errorMessage: '' };
-
+        // }
+    // the above constructor code is equivalent to
+    state = { lat: null, errorMessage: '' };
+    
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
             (position) => this.setState({lat: position.coords.latitude}),
             (error) => this.setState({errorMessage: error.message})
         );
     }
 
-    // this render method is must for React
-    render(){ // do not initialize in render method
+    renderContent(){
         if(this.state.errorMessage && !this.state.lat)
         return  <div>Error: {this.state.errorMessage}</div>;
 
         if(!this.state.errorMessage && this.state.lat)
-        return  <div>Latitude: {this.state.lat}</div>;
+        return  <SeasonDisplay lat={this.state.lat}/>
 
-        return  <div>Loading...</div>;
+        return  <Spinner message='Please accept location request'/>
+    }
+
+    // this render method is must for React
+    // Do not use multiple return statement inside a render function
+    render(){ // do not initialize in render method
+       return (
+           <div>{this.renderContent()}</div>
+       )
     }
 }
 ReactDOM.render(
