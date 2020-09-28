33045646e40e1a40170bedef76683cf2392e6905
ankur-jft <ankur.khandelwal@jellyfishtechnologies.com>
2020-09-28 21:26:06 +0530

    Video application completed

--- videos/src/api/youtube.js
+++ videos/src/api/youtube.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 const KEY=''
-axios.create({
-
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params:{
+        part:'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: `${KEY}`
+    }
 });
